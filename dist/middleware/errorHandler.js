61adaa48dc00c0130d6f4c313163905bdd7290cd
annguyen13796 <nguyenlaman.1307@gmail.com>
2022-12-19 03:39:56 +0700

    revert to commit 3fe53

--- dist/middleware/errorHandler.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const errorHandler = (err, req, res, next) => { };
-exports.default = errorHandler;
-//# sourceMappingURL=errorHandler.js.map
